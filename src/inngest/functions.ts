47673f517be66e5eea244af0762b526ed8b5f018
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-07-19 10:33:35 +0530

    Background Jobs and Sandbox Setup for code execution

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,1 +1,25 @@
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
+
+export const sandBoxTest = inngest.createFunction(
+  {
+    id: "sandbox-test",
+  },
+  {
+    event: "test/sandbox",
+  },
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("webly-nextjs-test2");
+      return sandbox.sandboxId;
+    });
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { sandboxUrl };
+  }
+);
