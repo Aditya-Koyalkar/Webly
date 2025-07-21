ef01673710a438a78f3490eb86abeefd51f71da4
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-07-21 20:09:12 +0530

    Messages UI

---
+++ src/modules/projects/ui/components/message-form.tsx
@@ -0,0 +1,100 @@
+import { Button } from "@/components/ui/button";
+import { Form, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { useTRPC } from "@/trpc/client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { ArrowUpIcon, Loader2Icon } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import TextareAutosize from "react-textarea-autosize";
+import { toast } from "sonner";
+import z from "zod";
+
+interface Props {
+  projectId: string;
+}
+
+const formschema = z.object({
+  value: z.string().min(1, { message: "Value is required" }).max(10000, { message: "Value too  long" }),
+});
+
+export const MessageForm = ({ projectId }: Props) => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const form = useForm<z.infer<typeof formschema>>({
+    defaultValues: {
+      value: "",
+    },
+    resolver: zodResolver(formschema),
+  });
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
+      onSuccess: () => {
+        form.reset();
+        queryClient.invalidateQueries(trpc.messages.getMany.queryOptions({ projectId }));
+      },
+      onError: (e) => {
+        toast.error(e.message);
+      },
+    })
+  );
+  const onSubmit = async (values: z.infer<typeof formschema>) => {
+    await createMessage.mutate({
+      projectId,
+      value: values.value,
+    });
+  };
+
+  const [isFocused, setIsFocused] = useState(false);
+  const showUsage = false;
+  const isDisabled = createMessage.isPending || !form.formState.isValid;
+
+  return (
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className={cn(
+            "relative border p-4 pt-1 rounded-xl bg-sidebar dark:bg-sidebar transition-all",
+            isFocused && "shadow-xs",
+            showUsage && "rounded-t-none"
+          )}
+        >
+          <FormField
+            name="value"
+            control={form.control}
+            render={({ field }) => (
+              <TextareAutosize
+                className="pt-4 resize-none border-none w-full outline-none bg-transparent"
+                {...field}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+                minRows={2}
+                maxRows={8}
+                placeholder="What would you like to build?"
+                onKeyDown={(e) => {
+                  if (e.key == "Enter" && (e.ctrlKey || e.metaKey)) {
+                    e.preventDefault();
+                    form.handleSubmit(onSubmit)(e);
+                  }
+                }}
+              />
+            )}
+          />
+          <div className="flex gap-x-2 items-end justify-between pt-2">
+            <div className="text-[10px] text-muted-foreground font-mono">
+              <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium">
+                <span>&#8984;</span> Enter
+              </kbd>
+              &nbsp; to submit
+            </div>
+            <Button disabled={isDisabled} className={cn("size-8 rounded-full", isDisabled && "bg-muted-foreground border")}>
+              {createMessage.isPending ? <Loader2Icon className="animate-spin" /> : <ArrowUpIcon />}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
--- src/modules/projects/ui/components/project-card.tsx
+++ src/modules/projects/ui/components/project-card.tsx
@@ -2,6 +2,8 @@ import { Card } from "@/components/ui/card";
 import { Fragment, MessageRole, MessageType } from "@/generated/prisma";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
+import { ChevronRightIcon, Code2Icon } from "lucide-react";
+import Image from "next/image";
 
 interface MessageCardProps {
   content: string;
@@ -46,14 +48,45 @@ interface AssistantMessageProps {
 }
 const AssistantMessage = ({ content, createdAt, fragment, isActiveFragment, onFragmentClick, type }: AssistantMessageProps) => {
   return (
-    <div className={cn("flex flex-col px-2 pb-6", type == "ERROR" && "text-red-700 dark:text-red-500")}>
+    <div className={cn("flex flex-col px-2 pb-6 group", type == "ERROR" && "text-red-700 dark:text-red-500")}>
       <div className="flex items-center gap-2 ">
+        <Image src={"/logo.svg"} alt="Webly" width={20} height={20} />
         <span className="text-sm font-medium">Webly</span>
-        <span className="text-sm text-muted-foreground opacity-0 transition-opacity">{format(createdAt, "HH:mm 'on' MMM dd, yyyy")}</span>
+        <span className="text-sm text-muted-foreground opacity-0 hover:opacity-100 group-hover:opacity-100 transition-opacity">
+          {format(createdAt, "HH:mm 'on' MMM dd, yyyy")}
+        </span>
       </div>
       <div className="pl-8 flex flex-col gap-y-5">
         <span>{content}</span>
+        {fragment && type == "RESULT" && <FragmentCard fragment={fragment} isActiveFragment={isActiveFragment} onFragmentClick={onFragmentClick} />}
       </div>
     </div>
+  );
+};
+
+interface FragmentCardProps {
+  fragment: Fragment;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+}
+
+const FragmentCard = ({ fragment, isActiveFragment, onFragmentClick }: FragmentCardProps) => {
+  return (
+    <button
+      onClick={() => onFragmentClick(fragment)}
+      className={cn(
+        "flex items-start text-start gap-2 border rounded-lg bg-muted w-fit p-3 hover:bg-secondary tracking-colors",
+        isActiveFragment && "bg-primary text-primary-foreground border-primary hover:bg-primary"
+      )}
+    >
+      <Code2Icon className="size-4 mt-0.5" />
+      <div className="flex flex-col flex-1">
+        <span className="text-sm font-medium line-clamp-1">{fragment.title}</span>
+        <span className="text-sm">Fragment Preview</span>
+      </div>
+      <div className="flex items-center justify-center">
+        <ChevronRightIcon className="size-4" />
+      </div>
+    </button>
   );
 };
